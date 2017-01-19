09dc3b15487e5c4d897f342b1bef9076cac1e144
jesse <jsoldat@hotmail.com>
2017-01-19 14:08:36 +0700

    can get data from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZ0Kt_NXjhwekjY7a_7KXPcMvu5cAjPWI",
+  authDomain: "angular2-c77fe.firebaseapp.com",
+  databaseURL: "https://angular2-c77fe.firebaseio.com",
+  storageBucket: "angular2-c77fe.appspot.com",
+  messagingSenderId: "155601858627"
+};
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
