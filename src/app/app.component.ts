e9d2cf2ae081ba1bad9235712b887bd6ccd8a91b
jesse <jsoldat@hotmail.com>
2017-01-19 17:18:37 +0700

    created json data and a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ FirebaseService ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire){
-  	this.items = af.database.list('/items');
-  	console.log(this.items);
+ 
+  businesses: Business[];
+  categories: Category[];
+
+  constructor(private firebaseService: FirebaseService){
+
   }
+
+  ngOnInit(){
+  	this.firebaseService.getCategories().subscribe(categories => {
+  		this.categories = categories
+  	});
+
+  	this.firebaseService.getBusinesses().subscribe(businesses => {
+  		this.businesses = businesses
+  	});
+  }
+} 
+
+
+export interface Business {
+	$key?: string;
+	company?: string;
+	description?: string;
+	category: string;
+	years_in_business?: number;
+	street_address?: string;
+	city?: string;
+	state?: string;
+	zipcode?: string;
+	phone?: string;
+	email?: string;
+	created_at: string;
+}
+
+export interface Category {
+	$key?: string;
+	name?: string;
 }
